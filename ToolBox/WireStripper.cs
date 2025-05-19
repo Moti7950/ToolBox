8b02b4b2040209122ae944ee5ffb77abb1224875
Moti <shalom7950@gmail.com>
2025-05-19 17:13:37 +0300

    Test commit

--- ToolBox/WireStripper.cs
+++ ToolBox/WireStripper.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ToolBox
 {
-    internal class WireStripper
+    internal class WireStripper: ToolBox
     {
+        public WireStripper(int weightKG):base("WireStripper", weightKG)
+        { }
+
+        public override void Describe()
+        {
+            Console.WriteLine("WeakReference - A class in C# that holds a reference to an object without preventing it from being garbage collected.");
+        }
+
+        public override void Use()
+        {
+            Console.WriteLine("WeakReference - Used to cache or reference large objects without blocking memory cleanup, helping manage memory efficiently.");
+        }
+
     }
 }
