794c7eda092de83d80db70deb0cd16d058c6e55d
Moti <shalom7950@gmail.com>
2025-05-19 17:11:22 +0300

    Updete Task 5 add 2 tools

---
+++ ToolBox/NailGun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox
+{
+    internal class NailGun: ToolBox
+    {
+        public NailGun(int weightKG):base ("NailGun",weightKG)
+        { }
+
+        public override void Describe()
+        {
+            Console.WriteLine("NailGun - A power tool that drives nails automatically using air pressure or electricity.");
+        }
+
+        public override void Use()
+        {
+            Console.WriteLine("NailGun - Quickly fastening pieces of wood in construction or carpentry.");
+        }
+    }
+}
